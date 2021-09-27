cc86d1f020c1bda0de8cc06947a22b1fc90fbacb
Carrington Jones <carrington.jones47@gmail.com>
2021-09-27 13:59:23 -0500

    Added delete button to counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,13 +11,14 @@ class Counter extends Component {
     };
 
     render() {
-
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={() => this.handleIncrement()}
                         className="btn btn-secondary btn-sm">Increment
                 </button>
+
+                <button className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
