e43ab569e5a6b76b834ef69143eb364f713ad053
Carrington Jones <carrington.jones47@gmail.com>
2021-09-28 12:06:17 -0500

    Added increment counter to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,9 @@ class NavBar extends Component {
         return (
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">Navbar
+                        <span className="Badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+                    </a>
                 </div>
             </nav>)
     }
